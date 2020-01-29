f7b51e72439b7f92b43f11590b113dac8d20da90
Jose Carlos Ramirez Moreno <jramirezm@aic.pgjguanajuato.gob.mx>
2020-01-29 12:53:18 -0600

    Commit inicial

---
+++ app_old.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+http.createServer( (req, res) => {
+
+    res.writeHead( 200, { 'Content-type': 'application/json' });
+    let salida = {
+        nombre: 'Jose Carlos',
+        edad: 28,
+        url: req.url
+    }
+    //res.write('Hola Mundo!') ;
+    res.write( JSON.stringify(salida));
+    res.end();
+})
+.listen(3000);
+
+console.log('Escuchando el puerto 3000');
